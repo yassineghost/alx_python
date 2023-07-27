108c61b671ddde8d8cf64b8b4ea6037fef312e63
yassine <yassine.moutawakil00@gmail.com>
2023-07-27 20:35:29 +0100

    4. Fibonacci Sequence Function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,13 @@
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        sequence = [0, 1]
+        for i in range(2, n):
+            next_num = sequence[i-1] + sequence[i-2]
+            sequence.append(next_num)
+        return sequence
