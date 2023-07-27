210e6b1e272c14ef9e342f607b99bc2e9cd8eb16
yassine <yassine.moutawakil00@gmail.com>
2023-07-27 20:20:42 +0100

    1. a ^ b

---
+++ python-functions/1-power.py
@@ -0,0 +1,19 @@
+def pow(a, b):
+    if a == 0 and b < 0:
+        raise ValueError("Cannot raise 0 to a negative power")
+
+    result = 1
+
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    while b > 0:
+        if b % 2 == 1:
+            result *= a
+        a *= a
+        b //= 2
+
+    result = round(result, 15)
+
+    return result
